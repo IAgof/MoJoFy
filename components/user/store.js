9976785cc0a21072d42c44c71de32ed7f0b47c9c
Carlos Hernandez <carloshdmn@gmail.com>
2017-09-26 11:58:54 +0200

    Add user upsert

--- components/user/store.js
+++ components/user/store.js
@@ -3,8 +3,25 @@ const Persistent = require('../../store/datastore');
 // const Cache = require('../../store/redis');
 
 exports.get = get;
+exports.upsert = upsert;
+
 
 function get(id, callback) {
+
+	// Cache.get('user', id, function(data) {
+	// 	if(!data) {
+			Persistent.get('user', id, function(data) {
+				callback(data);
+			});
+	// 	} else {
+	// 		callback(data);
+	// 	}
+	// });
+}
+
+function upsert(data, callback) {
+
+	const id = data.id || null;
 
 	// Cache.get('user', id, function(data) {
 	// 	if(!data) {
