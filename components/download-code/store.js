37fca94a1e5201fc959074fd7946caedbc3e4ceb
Carlos Hernandez <carloshdmn@gmail.com>
2018-02-06 08:55:10 +0100

    Add download code generation
    Also renamed some parameters, to improve readibility

--- components/download-code/store.js
+++ components/download-code/store.js
@@ -4,10 +4,22 @@ const type = 'download_code';
 
 
 exports.query = query;
+exports.upsert = upsert;
 
 
 function query(params, callback) {
 	Persistent.query(type, params, function(data) {
 		callback(data);
 	});
+}
+
+function upsert(data, callback) {
+	const id = data.id || data._id || null;
+	delete data.id;
+	delete data._id;
+
+	Persistent.add(type, data, id, function(result, id) {
+		callback(result, id);
+	});
+
 }
